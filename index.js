41b3b2a09cce83e49682ae6942c526f5806ed63b
antonypadela <71530505+sans-undertae@users.noreply.github.com>
2021-10-21 08:30:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var version = "1.1.2"
 var HttpClientGet = function (aUrl, aCallback) {
   let header = new Headers();
   header.append("Origin", window.location);
-  fetch(aUrl, {headers: header,})
+  fetch("https://cors-anywhere-sf.herokuapp.com/"+aUrl, {headers: header,})
     .then((response) => response.json())
     .then((json) => aCallback(json))
-    .catch((error) => console.log("Request failed : " + error.message + "\nURL: "+aUrl));
+    .catch((error) => console.log("Request failed : " + error.message + "\nURL: https://cors-anywhere-sf.herokuapp.com/"+aUrl));
 };
 
 window.addEventListener("load", function () {
@@ -42,21 +42,21 @@ window.addEventListener("load", function () {
       clone.innerHTML =`<span class="thumbnail-2d-container game-card-thumb-container"><img id = "img` +Place.id +`" class="" src="./content/cd.png" alt="` +Place.description +`" title="` +Place.name +`"></span><div class="game-card-name game-name-title" style="text-overflow: ellipsis;overflow: hidden;" title="` +Place.description +`">` +Place.name +`</div><div class="game-card-info"><img src="content/thumb.png" width="12" height="12"><span class="info-label vote-percentage-label" id = "like`+ Place.id +`">100%</span><img src="content/plr.png" width="12" height="12"><span class="info-label playing-counts-label" id = "plr`+ Place.id +`">0</span></div>`;
     }
 
-    HttpClientGet("https://cors-anywhere-sf.herokuapp.com/https://thumbnails.roblox.com/v1/games/icons?universeIds=" + PIDs.substr(1) + "&size=150x150&format=Png&isCircular=false",
+    HttpClientGet("https://thumbnails.roblox.com/v1/games/icons?universeIds=" + PIDs.substr(1) + "&size=150x150&format=Png&isCircular=false",
       function (json) {
         for (var i = 0; i < json.data.length; i++) {
           document.getElementById("img" + json.data[i].targetId).src = json.data[i].imageUrl || "./content/cd.png";
         }
       }
     );
-    HttpClientGet("https://cors-anywhere-sf.herokuapp.com/https://games.roblox.com/v1/games?universeIds=" + PIDs.substr(1),
+    HttpClientGet("https://games.roblox.com/v1/games?universeIds=" + PIDs.substr(1),
       function (json) {
         for (var i = 0; i < json.data.length; i++) {
           document.getElementById("plr" + json.data[i].id).innerHTML = json.data[i].playing;
         }
       }
     );
-    HttpClientGet("https://cors-anywhere-sf.herokuapp.com/https://games.roblox.com/v1/games/votes?universeIds=" + PIDs.substr(1),
+    HttpClientGet("https://games.roblox.com/v1/games/votes?universeIds=" + PIDs.substr(1),
       function (json) {
         for (var i = 0; i < json.data.length; i++) {
           perc = json.data[i].upVotes/(json.data[i].upVotes+json.data[i].downVotes)*100
@@ -70,8 +70,8 @@ window.addEventListener("load", function () {
 
     var ID = (uid.value.length > 0 && uid.value) || uid.placeholder;
     document.getElementById("pfp").src ="https://www.roblox.com/headshot-thumbnail/image?&width=150&height=150&format=png&userId=" +ID;
-    HttpClientGet("https://cors-anywhere-sf.herokuapp.com/https://games.roblox.com/v2/users/" + ID +"/games?limit=50&cursor="+(cursorToSearch || ""), UpdatePage)
-    HttpClientGet("https://cors-anywhere-sf.herokuapp.com/https://users.roblox.com/v1/users/" + ID,
+    HttpClientGet("https://games.roblox.com/v2/users/" + ID +"/games?limit=50&cursor="+(cursorToSearch || ""), UpdatePage)
+    HttpClientGet("https://users.roblox.com/v1/users/" + ID,
       function (json) {
         document.getElementById("name").innerHTML = "<a href='https://www.roblox.com/users/"+ID+"/profile' style='text-decoration: none;'><i>"+json.name+"</i></a>" + "'s Places";
       }
